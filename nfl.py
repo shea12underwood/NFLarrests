a8d31497478de83a0456274370fcfd6b092ece68
Shea Underwood <shea12underwood@gmail.com>
2019-12-15 22:50:18 -0500

    created the python file to call an api and convert it to a csv

---
+++ nfl.py
@@ -0,0 +1,21 @@
+import urllib.request
+import json
+import csv
+
+url =  "http://nflarrest.com/api/v1/crime?limit=20"
+f = urllib.request.urlopen(url)
+data = f.read().decode("utf-8")
+readfile = json.loads(data)
+
+with open('arrestdata.csv','w',newline = '') as arrestdata:
+    csvwriter =  csv.writer(arrestdata)
+    count = 0
+    for result in readfile:
+        if count ==0:
+            header = result.keys()
+            csvwriter.writerow(header)
+            count+=1
+        
+        csvwriter.writerow(result.values())
+
+
